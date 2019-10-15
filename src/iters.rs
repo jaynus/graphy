fde11279932b18be42bad24b45ae0796cc4a5083
jaynus <jaynus@gmail.com>
2019-10-15 07:41:24 -0700

    Save

--- src/iters.rs
+++ src/iters.rs
@@ -1,8 +1,98 @@
-use crate::{Graph, EdgeType, NodeType, Node, Edge};
+use crate::{Edge, EdgeType, Graph, Index, Node, NodeIndex, NodeType};
+use smallvec::SmallVec;
+use std::{marker::PhantomData, ops::Range};
+
+#[derive(Debug)]
+pub enum RelationshipRef<'a, N, E> {
+    Parent(&'a Edge<E>, &'a Node<N>),
+    Child(&'a Edge<E>, &'a Node<N>),
+}
+
+pub struct NeighborsRefIter<'a, N, E, I>
+where
+    I: Iterator<Item = RelationshipRef<'a, N, E>>,
+{
+    graph: &'a Graph<'a, N, E>,
+    iter: I,
+}
+impl<'a, N: NodeType, E: EdgeType, I> NeighborsRefIter<'a, N, E, I>
+where
+    I: Iterator<Item = RelationshipRef<'a, N, E>>,
+{
+    pub(crate) fn new(graph: &'a Graph<'a, N, E>, iter: I) -> Self {
+        Self { graph, iter }
+    }
+}
+impl<'a, N: NodeType, E: EdgeType, I> Iterator for NeighborsRefIter<'a, N, E, I>
+where
+    I: Iterator<Item = RelationshipRef<'a, N, E>>,
+{
+    type Item = RelationshipRef<'a, N, E>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, Some(self.graph.nodes.len()))
+    }
+}
+/*
+impl<'a, N: NodeType, E: EdgeType> DoubleEndedIterator for NeighborsRefIter<'a, N, E> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.cur -= 1;
+        self.graph.nodes.get(self.cur).map(|node| &**node)
+    }
+}
+impl<'a, N: NodeType, E: EdgeType> ExactSizeIterator for NeighborsRefIter<'a, N, E> {}
+*/
+
+pub struct IndexIter<I> {
+    range: Range<usize>,
+    cur: usize,
+    _marker: PhantomData<I>,
+}
+impl<I: Index> IndexIter<I> {
+    pub(crate) fn new(len: usize) -> Self {
+        Self {
+            range: Range { start: 0, end: len },
+            cur: 0,
+            _marker: Default::default(),
+        }
+    }
+}
+impl<I: Index> Iterator for IndexIter<I> {
+    type Item = I;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cur >= self.range.end {
+            None
+        } else {
+            let index = I::new(self.cur as u32);
+            self.cur += 1;
+            Some(index)
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, Some(self.range.end))
+    }
+}
+impl<I: Index> DoubleEndedIterator for IndexIter<I> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        unimplemented!()
+    }
+}
+impl<I: Index> ExactSizeIterator for IndexIter<I> {}
 
 pub struct NodeIter<'a, N, E> {
-    pub(crate) graph: &'a Graph<'a, N, E>,
-    pub(crate) cur: usize,
+    graph: &'a Graph<'a, N, E>,
+    cur: usize,
+}
+impl<'a, N: NodeType, E: EdgeType> NodeIter<'a, N, E> {
+    pub(crate) fn new(graph: &'a Graph<'a, N, E>) -> Self {
+        Self { graph, cur: 0 }
+    }
 }
 impl<'a, N: NodeType, E: EdgeType> Iterator for NodeIter<'a, N, E> {
     type Item = &'a Node<N>;
@@ -25,8 +115,13 @@ impl<'a, N: NodeType, E: EdgeType> DoubleEndedIterator for NodeIter<'a, N, E> {
 impl<'a, N: NodeType, E: EdgeType> ExactSizeIterator for NodeIter<'a, N, E> {}
 
 pub struct EdgeIter<'a, N, E> {
-    pub(crate) graph: &'a Graph<'a, N, E>,
-    pub(crate) cur: usize,
+    graph: &'a Graph<'a, N, E>,
+    cur: usize,
+}
+impl<'a, N: NodeType, E: EdgeType> EdgeIter<'a, N, E> {
+    pub(crate) fn new(graph: &'a Graph<'a, N, E>) -> Self {
+        Self { graph, cur: 0 }
+    }
 }
 impl<'a, N: NodeType, E: EdgeType> Iterator for EdgeIter<'a, N, E> {
     type Item = &'a Edge<E>;
@@ -53,12 +148,20 @@ pub struct NodeIterMut<'a, N, E> {
     pub(crate) graph: &'a mut Graph<'a, N, E>,
     pub(crate) cur: usize,
 }
+impl<'a, N: NodeType, E: EdgeType> NodeIterMut<'a, N, E> {
+    pub(crate) fn new(graph: &'a mut Graph<'a, N, E>) -> Self {
+        Self { graph, cur: 0 }
+    }
+}
 impl<'a, N: NodeType, E: EdgeType> Iterator for NodeIterMut<'a, N, E> {
     type Item = &'a mut Node<N>;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.cur += 1;
-        self.graph.nodes.get_mut(self.cur).map(|node| unsafe { &mut *(*node as *mut Node<N>) })
+        self.graph
+            .nodes
+            .get_mut(self.cur)
+            .map(|node| unsafe { &mut *(*node as *mut Node<N>) })
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
@@ -68,23 +171,33 @@ impl<'a, N: NodeType, E: EdgeType> Iterator for NodeIterMut<'a, N, E> {
 impl<'a, N: NodeType, E: EdgeType> DoubleEndedIterator for NodeIterMut<'a, N, E> {
     fn next_back(&mut self) -> Option<Self::Item> {
         self.cur -= 1;
-        self.graph.nodes.get_mut(self.cur).map(|node| unsafe { &mut *(*node as *mut Node<N>) })
+        self.graph
+            .nodes
+            .get_mut(self.cur)
+            .map(|node| unsafe { &mut *(*node as *mut Node<N>) })
     }
 }
 impl<'a, N: NodeType, E: EdgeType> ExactSizeIterator for NodeIterMut<'a, N, E> {}
 
-
-/// SAFETY: This iterator is safe because we are maintaing the mutable borrow on graph for the iterator
+/// SAFETY: This iterator is safe because we are maintaing the mutable borrow on graph f  or the iterator
 pub struct EdgeIterMut<'a, N, E> {
     pub(crate) graph: &'a mut Graph<'a, N, E>,
     pub(crate) cur: usize,
+}
+impl<'a, N: NodeType, E: EdgeType> EdgeIterMut<'a, N, E> {
+    pub(crate) fn new(graph: &'a mut Graph<'a, N, E>) -> Self {
+        Self { graph, cur: 0 }
+    }
 }
 impl<'a, N: NodeType, E: EdgeType> Iterator for EdgeIterMut<'a, N, E> {
     type Item = &'a mut Edge<E>;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.cur += 1;
-        self.graph.edges.get_mut(self.cur).map(|edge| unsafe { &mut *(*edge as *mut Edge<E>) })
+        self.graph
+            .edges
+            .get_mut(self.cur)
+            .map(|edge| unsafe { &mut *(*edge as *mut Edge<E>) })
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
@@ -94,7 +207,10 @@ impl<'a, N: NodeType, E: EdgeType> Iterator for EdgeIterMut<'a, N, E> {
 impl<'a, N: NodeType, E: EdgeType> DoubleEndedIterator for EdgeIterMut<'a, N, E> {
     fn next_back(&mut self) -> Option<Self::Item> {
         self.cur -= 1;
-        self.graph.edges.get_mut(self.cur).map(|edge| unsafe { &mut *(*edge as *mut Edge<E>) })
+        self.graph
+            .edges
+            .get_mut(self.cur)
+            .map(|edge| unsafe { &mut *(*edge as *mut Edge<E>) })
     }
 }
 impl<'a, N: NodeType, E: EdgeType> ExactSizeIterator for EdgeIterMut<'a, N, E> {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,15 @@
-use bumpalo::{Bump, collections::Vec as BumpVec};
-use std::fmt::Debug;
-use shrinkwraprs::Shrinkwrap;
-use smallvec::SmallVec;
-use derivative::Derivative;
-use std::sync::Arc;
-
 mod iters;
 
-pub trait NodeType : Debug + Clone + PartialEq {}
-impl<T> NodeType for T where T:  Debug + Clone + PartialEq {}
+use bumpalo::Bump;
+use derivative::Derivative;
+use smallvec::SmallVec;
+use std::{fmt::Debug, sync::Arc};
 
-pub trait EdgeType : Debug + Clone + PartialEq {}
-impl<T> EdgeType for T where T:  Debug + Clone + PartialEq {}
+pub trait NodeType: Debug + Clone + PartialEq {}
+impl<T> NodeType for T where T: Debug + Clone + PartialEq {}
+
+pub trait EdgeType: Debug + Clone + PartialEq {}
+impl<T> EdgeType for T where T: Debug + Clone + PartialEq {}
 
 // TODO: We should implement smallvec for BumpVec growth
 // Lifetimes make this hard for now so whatever
@@ -34,7 +32,8 @@ impl<T: NodeType> Node<T> {
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 pub enum Direction {
-    In, Out
+    In,
+    Out,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -50,14 +49,13 @@ pub struct Edge<T> {
 }
 
 impl<T: EdgeType> Edge<T> {
-    pub fn new(inner: T, from: NodeIndex, to: NodeIndex,) -> Self {
+    pub fn new(inner: T, from: NodeIndex, to: NodeIndex) -> Self {
         Self {
             inner,
-            nodes: EdgeNodes { from, to},
+            nodes: EdgeNodes { from, to },
         }
     }
 }
-
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 pub enum GraphError {
@@ -66,86 +64,183 @@ pub enum GraphError {
     InvalidIndex,
 }
 
-#[derive(Shrinkwrap, Debug, Default, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
-pub struct NodeIndex(usize);
+#[derive(Debug, Default, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub struct NodeIndex(u32);
+impl std::ops::Add for NodeIndex {
+    type Output = Self;
 
-#[derive(Shrinkwrap, Debug, Default, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
-pub struct EdgeIndex(usize);
+    fn add(self, other: Self) -> Self {
+        Self(self.0 + other.0)
+    }
+}
 
+#[derive(Debug, Default, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub struct EdgeIndex(u32);
+impl std::ops::Add for EdgeIndex {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self(self.0 + other.0)
+    }
+}
+
+pub trait Index {
+    fn new(value: u32) -> Self;
+    fn index(&self) -> usize;
+}
+impl Index for NodeIndex {
+    fn new(value: u32) -> Self {
+        Self(value)
+    }
+    fn index(&self) -> usize {
+        self.0 as usize
+    }
+}
+impl Index for EdgeIndex {
+    fn new(value: u32) -> Self {
+        Self(value)
+    }
+    fn index(&self) -> usize {
+        self.0 as usize
+    }
+}
 #[derive(Derivative, Debug)]
 #[derivative(Default(bound = ""))]
 pub struct Graph<'a, N, E> {
     nodes: SmallVec<[&'a mut Node<N>; 128]>,
     edges: SmallVec<[&'a mut Edge<E>; 256]>,
 }
-impl <'a, N: NodeType, E: EdgeType> Graph<'a, N, E> {
+impl<'a, N: NodeType, E: EdgeType> Graph<'a, N, E> {
+    pub fn insert_node(
+        &mut self,
+        allocator: &'a GraphAllocator,
+        inner: N,
+    ) -> Result<NodeIndex, GraphError> {
+        let index = NodeIndex(self.edges.len() as u32);
 
-    pub fn insert_node(&mut self, allocator: &'a GraphAllocator, inner: N) -> Result<NodeIndex, GraphError> {
-        let index = self.edges.len();
         self.nodes.push(allocator.0.alloc(Node::new(inner)));
-        Ok(NodeIndex(index))
-    }
-
-    pub fn insert_edge<F>(&mut self, allocator: &'a GraphAllocator, inner: E, from: NodeIndex, to: NodeIndex, cyclic_fn: F) -> Result<EdgeIndex, GraphError>
-        where F: FnOnce(Self, E) -> Result<EdgeIndex, GraphError>
-    {
-        unimplemented!()
-    }
-
-    pub fn insert_edge_unchecked(&mut self, allocator: &'a GraphAllocator, inner: E, from: NodeIndex, to: NodeIndex) -> Result<EdgeIndex, GraphError> {
-        let index = EdgeIndex(self.edges.len());
-        self.edges.push(allocator.0.alloc(Edge::new(inner, from, to)));
-
-        self.nodes.get_mut(*from).ok_or_else(|| GraphError::InvalidIndex)?.outgoing.push(index);
-        self.nodes.get_mut(*to).ok_or_else(|| GraphError::InvalidIndex)?.incoming.push(index);
 
         Ok(index)
     }
 
-    pub fn update_edge(&mut self, edge_index: EdgeIndex, from: Option<NodeIndex>, to: Option<NodeIndex>) -> Result<(), GraphError> {
-        let edge = self.edges.get_mut(*edge_index).ok_or_else(|| GraphError::InvalidIndex)?;
+    pub fn insert_edge<F>(
+        &mut self,
+        allocator: &'a GraphAllocator,
+        inner: E,
+        from: NodeIndex,
+        to: NodeIndex,
+        cyclic_fn: F,
+    ) -> Result<EdgeIndex, GraphError>
+    where
+        F: FnOnce(Self, E) -> Result<EdgeIndex, GraphError>,
+    {
+        unimplemented!()
+    }
+
+    pub fn insert_edge_unchecked(
+        &mut self,
+        allocator: &'a GraphAllocator,
+        inner: E,
+        from: NodeIndex,
+        to: NodeIndex,
+    ) -> Result<EdgeIndex, GraphError> {
+        let index = EdgeIndex(self.edges.len() as u32);
+
+        self.edges
+            .push(allocator.0.alloc(Edge::new(inner, from, to)));
+
+        self.nodes
+            .get_mut(from.index())
+            .ok_or_else(|| GraphError::InvalidIndex)?
+            .outgoing
+            .push(index);
+        self.nodes
+            .get_mut(to.index())
+            .ok_or_else(|| GraphError::InvalidIndex)?
+            .incoming
+            .push(index);
+
+        Ok(index)
+    }
+
+    pub fn update_edge(
+        &mut self,
+        edge_index: EdgeIndex,
+        from: Option<NodeIndex>,
+        to: Option<NodeIndex>,
+    ) -> Result<(), GraphError> {
+        let edge = self
+            .edges
+            .get_mut(edge_index.index())
+            .ok_or_else(|| GraphError::InvalidIndex)?;
 
         if let Some(from) = from {
-            self.nodes.get_mut(*edge.nodes.from).ok_or_else(|| GraphError::InvalidIndex)?.outgoing.remove(*edge_index);
+            self.nodes
+                .get_mut(edge.nodes.from.index())
+                .ok_or_else(|| GraphError::InvalidIndex)?
+                .outgoing
+                .remove(edge_index.index());
             edge.nodes.from;
         }
         if let Some(to) = to {
-            self.nodes.get_mut(*edge.nodes.to).ok_or_else(|| GraphError::InvalidIndex)?.incoming.remove(*edge_index);
+            self.nodes
+                .get_mut(edge.nodes.to.index())
+                .ok_or_else(|| GraphError::InvalidIndex)?
+                .incoming
+                .remove(edge_index.index());
             edge.nodes.to;
         }
 
         Ok(())
     }
 
-    pub fn nodes_iter(&'a self) -> iters::NodeIter<'a, N, E> {
-        iters::NodeIter { graph: self, cur: 0 }
+    pub fn nodes_iter(&'a self) -> impl Iterator<Item = &'a Node<N>> {
+        iters::NodeIter::new(self)
     }
-    pub fn nodes_iter_mut(&'a mut self) -> iters::NodeIterMut<'a, N, E> {
-        iters::NodeIterMut { graph: self, cur: 0 }
+    pub fn nodes_iter_mut(&'a mut self) -> impl Iterator<Item = &'a mut Node<N>> {
+        iters::NodeIterMut::new(self)
     }
 
-
-    pub fn edges_iter(&'a self) -> iters::EdgeIter<'a, N, E> {
-        iters::EdgeIter { graph: self, cur: 0 }
+    pub fn edges_iter(&'a self) -> impl Iterator<Item = &'a Edge<E>> {
+        iters::EdgeIter::new(self)
     }
-    pub fn edges_iter_mut(&'a mut self) -> iters::EdgeIterMut<'a, N, E> {
-        iters::EdgeIterMut { graph: self, cur: 0 }
+    pub fn edges_iter_mut(&'a mut self) -> impl Iterator<Item = &'a mut Edge<E>> {
+        iters::EdgeIterMut::new(self)
+    }
+
+    pub fn nodes_indices_iter(&'a self) -> impl Iterator<Item = NodeIndex> {
+        iters::IndexIter::<NodeIndex>::new(self.nodes.len())
+    }
+    pub fn edges_indices_iter(&'a self) -> impl Iterator<Item = EdgeIndex> {
+        iters::IndexIter::<EdgeIndex>::new(self.edges.len())
     }
 
     // Return false from the visitor to stop this branches traversal
     pub fn visit<V>(&self, node_index: NodeIndex, visitor: V) -> Result<(), GraphError>
-        where V: FnMut(&Graph<N, E>, &Node<N>, &Edge<E>) -> bool
+    where
+        V: FnMut(&Graph<N, E>, &Node<N>, &Edge<E>) -> bool,
     {
         self.visit_traverse(node_index, Arc::new(visitor))
     }
 
     // Return false from the visitor to stop this branches traversal
-    fn visit_traverse<V>(&self, node_index: NodeIndex, mut visitor: Arc<V>) -> Result<(), GraphError>
-        where V: FnMut(&Graph<N, E>, &Node<N>, &Edge<E>) -> bool
+    fn visit_traverse<V>(
+        &self,
+        node_index: NodeIndex,
+        mut visitor: Arc<V>,
+    ) -> Result<(), GraphError>
+    where
+        V: FnMut(&Graph<N, E>, &Node<N>, &Edge<E>) -> bool,
     {
-        let node = self.nodes.get(*node_index).ok_or_else(|| GraphError::InvalidIndex)?;
+        let node = self
+            .nodes
+            .get(node_index.index())
+            .ok_or_else(|| GraphError::InvalidIndex)?;
         for edge_index in &node.outgoing {
-            let edge = self.edges.get(**edge_index).ok_or_else(|| GraphError::InvalidIndex)?;
+            let edge = self
+                .edges
+                .get(edge_index.index())
+                .ok_or_else(|| GraphError::InvalidIndex)?;
             if (Arc::get_mut(&mut visitor).unwrap())(self, node, edge) {
                 self.visit_traverse(edge.nodes.to, visitor.clone())?;
             }
@@ -154,32 +249,64 @@ impl <'a, N: NodeType, E: EdgeType> Graph<'a, N, E> {
         Ok(())
     }
 
+    pub fn get_neighbors(
+        &'a self,
+        node_index: NodeIndex,
+    ) -> impl Iterator<Item = iters::RelationshipRef<N, E>> {
+        iters::NeighborsRefIter::new(
+            self,
+            self.nodes
+                .get(node_index.index())
+                .unwrap()
+                .incoming
+                .iter()
+                .map(move |edge_index| {
+                    let edge: &Edge<E> = self.edges.get(edge_index.index()).unwrap();
+                    let node = self.nodes.get(edge.nodes.from.index()).unwrap();
+                    iters::RelationshipRef::Parent(edge, node)
+                })
+                .chain(
+                    self.nodes
+                        .get(node_index.index())
+                        .unwrap()
+                        .outgoing
+                        .iter()
+                        .map(move |edge_index| {
+                            let edge: &Edge<E> = self.edges.get(edge_index.index()).unwrap();
+                            let node = self.nodes.get(edge.nodes.from.index()).unwrap();
+                            iters::RelationshipRef::Child(edge, node)
+                        }),
+                ),
+        )
+    }
+
     pub fn get_node(&self, node_index: NodeIndex) -> Option<&Node<N>> {
-        self.nodes.get(*node_index).map(|node| &**node)
+        self.nodes.get(node_index.index()).map(|node| &**node)
     }
 
     pub fn get_node_mut(&mut self, node_index: NodeIndex) -> Option<&mut Node<N>> {
-        self.nodes.get_mut(*node_index).map(|node| &mut **node)
+        self.nodes
+            .get_mut(node_index.index())
+            .map(|node| &mut **node)
     }
 
-
     pub fn get_edge(&self, node_index: NodeIndex) -> Option<&Edge<E>> {
-        self.edges.get(*node_index).map(|edge| &**edge)
+        self.edges.get(node_index.index()).map(|edge| &**edge)
     }
 
     pub fn get_edge_mut(&mut self, node_index: NodeIndex) -> Option<&mut Edge<E>> {
-        self.edges.get_mut(*node_index).map(|edge| &mut **edge)
+        self.edges
+            .get_mut(node_index.index())
+            .map(|edge| &mut **edge)
     }
 
-
     // TODO:
-    fn reset(&mut self, allocator: &'a mut GraphAllocator, ) {
+    fn reset(&mut self, allocator: &'a mut GraphAllocator) {
         allocator.0.reset();
         self.nodes.clear();
         self.edges.clear();
     }
 }
-
 
 #[derive(Debug, Default)]
 pub struct GraphAllocator(Bump);
